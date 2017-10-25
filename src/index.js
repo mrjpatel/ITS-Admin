cce5d3a1920562354b1ed07d4be8e8780b7783e6
Japan Patel <japanpatel1198@gmail.com>
2017-10-25 15:55:24 +1100

    Added instructions for fire base api

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase';
 import {BrowserRouter} from "react-router-dom";
 
-const config = {
+/*const config = {
     apiKey: "AIzaSyCdmTEy9vmZItRtWQDgimYMZAkeo2zqgzE",
     authDomain: "test1-fda03.firebaseapp.com",
     databaseURL: "https://test1-fda03.firebaseio.com",
     projectId: "test1-fda03",
     storageBucket: "test1-fda03.appspot.com",
     messagingSenderId: "73822061507"
-};
+};*/
+//Copy this code from the google firebase
 firebase.initializeApp(config);
 ReactDOM.render(
     <BrowserRouter>
